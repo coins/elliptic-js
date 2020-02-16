36df120d80e530818b47f27fa80f0b73f78e3810
RobinLinus <robinlinus@users.noreply.github.com>
2020-02-16 02:59:53 +0100

    Clean up DER-encoding

--- src/signature-DER/signature-DER.js
+++ src/signature-DER/signature-DER.js
@@ -3,28 +3,59 @@ import { SerialBuffer, Uint8, toBigInt, fromBigInt } from '../../../buffer-js/bu
 
 /**
  * 
- * DER encoding for ECC signatures
+ * Class for DER encoded ECC signatures.
  * @see https://bitcoin.stackexchange.com/questions/77191/what-is-the-maximum-size-of-a-der-encoded-ecdsa-signature
  * @see https://github.com/libbitcoin/libbitcoin-system/wiki/Sighash-and-TX-Signing
  */
 export class SignatureDER extends SerialBuffer {
 
+    /**
+     * The r-value of the signature.
+     * @type {Uint8Array}
+     */
+    rValue
+
+    /**
+     * The s-value of the signature.
+     * @type {Uint8Array}
+     */
+    sValue
+
+    /** 
+     * Convert a pair of Uint8Arrays to a DER encoded Signature.
+     * @param  {Uint8Array} rValue - The r-value of the signature.
+     * @param  {Uint8Array} sValue - The s-value of the signature.
+     */
     constructor(rValue, sValue) {
         super()
-        this.rValue = rValue // TODO: represent as BigInt instead of byte array
-        this.sValue = sValue // TODO: represent as BigInt instead of byte array
+        this.rValue = rValue
+        this.sValue = sValue
     }
 
+    /** 
+     * Convert a pair of BigInt to a DER encoded Signature.
+     * @param  {BigInt} r - The r-value of the signature.
+     * @param  {BigInt} s - The s-value of the signature.
+     * @return {SignatureDER}
+     */
     static fromBigInts(r, s) {
         r = fromBigInt(r)
         s = fromBigInt(s)
         return new SignatureDER(r, s)
     }
 
+    /** 
+     * The signatures "s"-value as BigInt
+     * @return {BigInt}
+     */
     get s() {
         return toBigInt(this.sValue)
     }
 
+    /** 
+     * The signatures "r"-value as BigInt
+     * @return {BigInt}
+     */
     get r() {
         return toBigInt(this.rValue)
     }
@@ -33,48 +64,69 @@ export class SignatureDER extends SerialBuffer {
      * @override
      */
     write(writer) {
-        // write the "tag"
-        const tag = new Uint8(48)
+        // "DER prefix tag"
+        const tag = new Uint8(0x30)
         tag.write(writer)
 
-        // write the sequence length
+        // sequence length
         const sequenceLength = new Uint8(this.rValue.byteLength + this.sValue.byteLength + 4)
         sequenceLength.write(writer)
 
-        writer.writeByte(2) // integerElement1
-        writer.writeByte(this.rValue.byteLength) // element1Length
+        // marker for r value
+        writer.writeByte(2)
+        // length of the r value
+        writer.writeByte(this.rValue.byteLength)
+        // the r value
         writer.writeBytes(this.rValue)
 
-        writer.writeByte(2) // integerElement2
-        writer.writeByte(this.sValue.byteLength) // element2Length
+        // marker for s value
+        writer.writeByte(2)
+        // length of the s value
+        writer.writeByte(this.sValue.byteLength)
+        // the s value
         writer.writeBytes(this.sValue)
-        
-        writer.writeByte(1) // SigHashFlag // TODO: actually read it!
-    }
-
-    /**
-     * @override
-     */
-    byteLength() {
-        return this.rValue.byteLength + this.sValue.byteLength + 4 + 2 + 1
     }
 
     /**
      * @override
      */
     static read(reader) {
+        // "DER prefix tag"
         const tag = Uint8.read(reader)
+        // sequence length
         const sequenceLength = Uint8.read(reader)
-        const integerElement1 = Uint8.read(reader)
-        const elementLength1 = Uint8.read(reader)
-        const rValue = reader.readBytes(elementLength1)
 
-        const integerElement2 = Uint8.read(reader)
-        const elementLength2 = Uint8.read(reader)
-        const sValue = reader.readBytes(elementLength2)
-        
-        Uint8.read(reader) // SigHashFlag // TODO: actually read it!
+        // marker for r value
+        const rValueMarker = Uint8.read(reader)
+        // length of the r value
+        const rValueLength = Uint8.read(reader)
+        // the r value
+        const rValue = reader.readBytes(rValueLength)
+
+        // marker for s value
+        const sValueMarker = Uint8.read(reader)
+        // length of the s value
+        const sValueLength = Uint8.read(reader)
+        // the s value
+        const sValue = reader.readBytes(sValueLength)
 
         return new SignatureDER(rValue, sValue)
+    }
+
+    /**
+     * @override
+     */
+    byteLength() {
+        /*
+            1 byte - DER prefix tag
+            1 byte - sequenceLength
+            1 byte - marker for r value
+            1 byte - length of the r value
+            1 byte - marker for s value
+            1 byte - length of the s value
+            --------------------------
+            6 bytes
+         */
+        return this.rValue.byteLength + this.sValue.byteLength + 6
     }
 }
--- src/signature-DER/test.js
+++ src/signature-DER/test.js
@@ -3,11 +3,20 @@ import { SignatureDER } from './signature-DER.js'
 describe('Signature DER (distinguished encoding rules)', function() {
 
     it('can encode and decode a signature', async function() {
-    	const sigHashFlag = '01'
-        const encodedExample = '304402206a2eb16b7b92051d0fa38c133e67684ed064effada1d7f925c842da401d4f22702201f196b10e6e4b4a9fff948e5c5d71ec5da53e90529c8dbd122bff2b1d21dc8a9' + sigHashFlag
+        const encodedExample = '304402206a2eb16b7b92051d0fa38c133e67684ed064effada1d7f925c842da401d4f22702201f196b10e6e4b4a9fff948e5c5d71ec5da53e90529c8dbd122bff2b1d21dc8a9'
+        const decoded = SignatureDER.fromHex(encodedExample)
+        const encoded = decoded.toHex()
+        expect(encoded).toBe(encodedExample)
+    })    
+
+
+    it('can encode and decode a signature', async function() {
+        const encodedExample = '3045022100afff580595971b8c1700e77069d73602aef4c2a760dbd697881423dfff845de80220579adb6a1ac03acde461b5821a049ebd39a8a8ebf2506b841b15c27342d2e342'
         const decoded = SignatureDER.fromHex(encodedExample)
         const encoded = decoded.toHex()
         expect(encoded).toBe(encodedExample)
     })
+
+
 
 })
